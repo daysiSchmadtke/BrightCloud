aef25cd125a651bba478604601eeb9a9301eefe6
Daysi Schmadtke <daysischmadtke@gmail.com>
2025-05-22 20:18:25 -0400

    delete

--- src/app/clients/new/page.js
+++ src/app/clients/new/page.js
@@ -1,5 +1,17 @@
+'use client';
+
 import ClientForm from '@/components/forms/ClientForm';
+import { useRouter } from 'next/navigation';
+import { getClients } from '@/api/clientsData';
 
 export default function AddClient() {
-  return <ClientForm />;
+  const router = useRouter();
+
+  const handleUpdate = async () => {
+    console.log('Refreshing client list...');
+    await getClients();
+    router.push('/');
+  };
+
+  return <ClientForm onUpdate={handleUpdate} onClose={() => router.push('/')} />;
 }
